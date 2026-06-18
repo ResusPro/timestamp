dddf5c5214abea02fb011fb7b1cf359a0b5507ea
ResusPro <dr.nick.foster80@gmail.com>
2026-06-18 02:41:20 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "timestamp-logger-v0-4-0";
+const CACHE_NAME = "timestamp-logger-v0-5-0";
 const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
